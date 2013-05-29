9b5e8297746ca67e6a766702e4bfd969b69c987c
victormalli <victormalli@gmail.com>
2013-05-29 18:23:29 +0530

    New application

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyFirstiOSApplication
+//
+//  Created by NagaMalleswar on 25/05/13.
+//  Copyright (c) 2013 MyOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyFirstiOSApplication
+//
+//  Created by NagaMalleswar on 25/05/13.
+//  Copyright (c) 2013 MyOrganization. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
